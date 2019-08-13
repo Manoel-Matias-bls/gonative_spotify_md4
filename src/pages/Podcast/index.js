9b1d0e3108c001cc488a90a429e2c3c6c100570b
Manoel-Matias-bls <manoelmatias.cardoso@gmail.com>
2019-08-13 09:20:28 -0300

    app finalizado

--- src/pages/Podcast/index.js
+++ src/pages/Podcast/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PlayerActions from '~/store/ducks/player';
+
 import {
   Container,
   EpisodeList,
@@ -17,15 +21,22 @@ import {
   Title,
 } from './styles';
 
-export default class Podcast extends Component {
+class Podcast extends Component {
   handleBack = () => {
     const { navigation } = this.props;
 
     navigation.goBack();
   };
 
+  handlePlay = episodeId => {
+    const { setPodcastRequest, navigation } = this.props;
+    const podcast = navigation.getParam('podcast');
+
+    setPodcastRequest(podcast, episodeId);
+  };
+
   render() {
-    const { navigation } = this.props;
+    const { navigation, currentEpisode } = this.props;
     const podcast = navigation.getParam('podcast');
 
     return (
@@ -42,7 +53,7 @@ export default class Podcast extends Component {
 
               <PodcastTitle>{podcast.title}</PodcastTitle>
 
-              <PlayButton onPress={() => {}}>
+              <PlayButton onPress={() => this.handlePlay()}>
                 <PlayButtonText>REPRODUZIR</PlayButtonText>
               </PlayButton>
             </PodcastDetails>
@@ -50,8 +61,10 @@ export default class Podcast extends Component {
           data={podcast.tracks}
           keyExtractor={episode => String(episode.id)}
           renderItem={({ item: episode }) => (
-            <Episode>
-              <Title>{episode.title}</Title>
+            <Episode onPress={() => this.handlePlay(episode.id)}>
+              <Title active={currentEpisode && currentEpisode.id === episode.id}>
+                {episode.title}
+              </Title>
               <Author>{episode.artist}</Author>
             </Episode>
           )}
@@ -60,3 +73,16 @@ export default class Podcast extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  currentEpisode: state.player.podcast
+    ? state.player.podcast.tracks.find(episode => episode.id === state.player.current)
+    : null,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Podcast);
